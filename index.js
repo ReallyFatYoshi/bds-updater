14d4a67dd4e0169dff4bafdbf87e7de5ca67674c
ReallyFatYoshi <86920416+ReallyFatYoshi@users.noreply.github.com>
2023-01-30 13:43:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ try {
         'curl -H "User-Agent:  Mozilla/5.0 (Windows NT 10.0; Win32; x32; rv:103.0) Gecko/30200202 Firefox/101" https://www.minecraft.net/en-us/download/server/bedrock',
         function (_, res) {
           // const download = res.match(
-            /https\:\/\/minecraft\.azureedge\.net\/bin-win\/bedrock-server-\d+\.\d+\.\d+\.\d+\.zip/gm
+          //  /https\:\/\/minecraft\.azureedge\.net\/bin-win\/bedrock-server-\d+\.\d+\.\d+\.\d+\.zip/gm
           )[0]?.replace(/http(s)?:\/\//g,"");
           const version = download.match(
             /(?<=(https\:\/\/)?minecraft\.azureedge\.net\/bin-win\/bedrock-server-)\d+\.\d+\.\d+\.\d+(?=\.zip)/gm
