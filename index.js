9420cd7df0169d7d9934107faf5d11616b7ab3bc
ReallyFatYoshi <86920416+ReallyFatYoshi@users.noreply.github.com>
2023-01-30 13:43:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ try {
         function (_, res) {
           // const download = res.match(
           //  /https\:\/\/minecraft\.azureedge\.net\/bin-win\/bedrock-server-\d+\.\d+\.\d+\.\d+\.zip/gm
-          )[0]?.replace(/http(s)?:\/\//g,"");
+          // )[0]?.replace(/http(s)?:\/\//g,"");
           const version = download.match(
             /(?<=(https\:\/\/)?minecraft\.azureedge\.net\/bin-win\/bedrock-server-)\d+\.\d+\.\d+\.\d+(?=\.zip)/gm
           )[0];
