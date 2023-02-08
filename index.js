81f9bd9345da0b8244e29f1c73e205d425c273ae
ReallyFatYoshi <86920416+ReallyFatYoshi@users.noreply.github.com>
2023-02-08 15:19:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,17 +5,27 @@ try {
     child_process.exec(
         'curl -H "User-Agent:  Mozilla/5.0 (Windows NT 10.0; Win32; x32; rv:103.0) Gecko/30200202 Firefox/101" https://www.minecraft.net/en-us/download/server/bedrock',
         function (_, res) {
-          const download = res.match(
-            /https\:\/\/minecraft\.azureedge\.net\/bin-win\/bedrock-server-\d+\.\d+\.\d+\.\d+\.zip/gm
+          const downloadStable = res.match(
+           /https\:\/\/minecraft\.azureedge\.net\/bin-win\/bedrock-server-(\d+(\.)?){3,4}\.zip/gm
           )[0]?.replace(/http(s)?:\/\//g,"");
-          const stableversion = download.match(
-            /(?<=(https\:\/\/)?minecraft\.azureedge\.net\/bin-win\/bedrock-server-)\d+\.\d+\.\d+\.\d+(?=\.zip)/gm
+          const downloadPreview = res.match(
+              /https\:\/\/minecraft\.azureedge\.net\/bin-win-preview\/bedrock-server-(\d+(\.)?){3,4}\.zip/gm
+          )[0]?.replace(/http(s)?:\/\//g,"");
+            
+          const stableversion = downloadStable.match(
+            /(?<=(https\:\/\/)?minecraft\.azureedge\.net\/bin-win-preview\/bedrock-server-)(\d+(\.)?){3,4}(?=\.zip)/gm
+          )[0];
+          const previewversion = downloadStable.match(
+            /(?<=(https\:\/\/)?minecraft\.azureedge\.net\/bin-win-preview\/bedrock-server-)(\d+(\.)?){3,4}(?=\.zip)/gm
           )[0];
       
           fs.writeFileSync(
             "./win/config.json",
             JSON.stringify({
-              preview: {},
+              preview: {
+                download: `https://minecraft.azureedge.net/bin-win-preview/bedrock-server-${previewversion}.zip`,
+                version: previewversion,
+              },
               stable: {
                 download: `https://minecraft.azureedge.net/bin-win/bedrock-server-${stableversion}.zip`,
                 version: stableversion,
@@ -28,7 +38,10 @@ try {
           fs.writeFileSync(
             "./linux/config.json",
             JSON.stringify({
-              preview: {},
+              preview: {
+                download: `https://minecraft.azureedge.net/bin-linux-preview/bedrock-server-${previewversion}.zip`,
+                version: previewversion,
+              },
               stable: {
                 download: `https://minecraft.azureedge.net/bin-linux/bedrock-server-${stableversion}.zip`,
                 version: stableversion,
